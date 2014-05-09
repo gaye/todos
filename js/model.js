a0c2444f176ef03229b0d65e155120da90ae2633
gaye <gaye@mozilla.com>
2014-05-08 20:53:19 -0400

    Initial commit

---
+++ js/model.js
@@ -0,0 +1,98 @@
+define(function(require) {
+  'use strict';
+
+  var api = require('api'),
+      ko = require('knockout');
+
+  function Model() {
+    this.queryToObservables = { done: [], wip: [] };
+    this.queryToCached = {};
+  }
+
+  Model.prototype = {
+    add: function(description) {
+      var todo = {
+        description: description,
+        done: false
+      };
+
+      api.add(todo);
+
+      // For now we have to "write through the cache"
+      // since indexedDB does not support change events.
+      if ('wip' in this.queryToCached) {
+        this.queryToCached.wip.push(todo);
+        this._publish('wip');
+      }
+    },
+
+    update: function(todo) {
+      api.update(todo);
+
+      // For now we have to "write through the cache"
+      // since indexedDB does not support change events.
+      var wip = this.queryToCached.wip,
+          done = this.queryToCached.done;
+      wip.some(function(value, index) {
+        if (value.id === todo.id) {
+          wip.splice(index, 1);
+          done.push(value);
+          this._publish('wip');
+          this._publish('done');
+          return true;
+        }
+      }.bind(this));
+    },
+
+    done: function() {
+      return this._subscribe('done');
+    },
+
+    wip: function() {
+      return this._subscribe('wip');
+    },
+
+    _subscribe: function(query) {
+      var result = ko.observableArray();
+      this.queryToObservables[query].push(result);
+
+      if (query in this.queryToCached) {
+        console.log('Cached!');
+        var cached = this.queryToCached[query];
+        setObservableArray(result, cached);
+      } else {
+        console.log('Cache miss!');
+        api[query]().then(function(fetched) {
+          console.log('Pulled ' + query + ' into cache, found ' + fetched);
+          this.queryToCached[query] = fetched;
+          this._publish(query);
+        }.bind(this));
+      }
+
+      return result;
+    },
+
+    _publish: function(query) {
+      var newValue = this.queryToCached[query];
+      var observables = this.queryToObservables[query];
+      console.log('Publishing ' + query + ' to ' +
+                  observables.length + ' subscribers.');
+      observables.forEach(function(observable) {
+        setObservableArray(observable, newValue);
+      });
+    }
+  };
+
+
+  /**
+   * Copy the data in one array to the other in lieu of =
+   */
+  function setObservableArray(writeTo, readFrom) {
+    writeTo.removeAll();
+    readFrom.forEach(function(value) {
+      writeTo.push(value);
+    });
+  }
+
+  return new Model();
+});
---
+++ js/views/wip.js
@@ -0,0 +1,22 @@
+define(function(require) {
+  var ko = require('knockout'),
+      model = require('model');
+
+  return function() {
+    var view = this;
+
+    view.todos = model.wip();
+
+    view.name = ko.observable('Type shit here!');
+
+    view.addTodo = function() {
+      model.add(view.name());
+      view.name('');
+    };
+
+    view.finishTodo = function() {
+      this.done = true;
+      model.update(this);
+    };
+  };
+});
